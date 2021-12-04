fb2df1b023b3b989ae5bb60724078e95d37173bd
Guillaume Gérard <1322081+GreatWizard@users.noreply.github.com>
2021-12-04 16:57:06 +0100

    chore: update ember version (#26)

--- app/controllers/club.js
+++ app/controllers/club.js
@@ -1,40 +1,40 @@
 import Controller from '@ember/controller';
 import { A } from '@ember/array';
-import env from '../config/environment';
+import env from 'site-aiki3/config/environment';
 
 const { rootURL } = env;
 
-export default Controller.extend({
-  gallery: A([
+export default class ClubController extends Controller {
+  gallery = A([
     {
       title: "Michel à droite, professeur de l'Aiki3",
       src: `${rootURL}assets/gallery_aiki1.jpg`,
       w: 479,
-      h: 328
+      h: 328,
     },
     {
       title: 'Kokyu Nage (projection)',
       src: `${rootURL}assets/gallery_aiki2.jpg`,
       w: 400,
-      h: 330
+      h: 330,
     },
     {
       title: 'Shihoo Nage (projection)',
       src: `${rootURL}assets/gallery_aiki3.jpg`,
       w: 400,
-      h: 330
+      h: 330,
     },
     {
       title: 'Hiji Kime Osae',
       src: `${rootURL}assets/gallery_aiki4.jpg`,
       w: 400,
-      h: 327
+      h: 327,
     },
     {
       title: 'Nikyo',
       src: `${rootURL}assets/gallery_aiki5.jpg`,
       w: 400,
-      h: 330
-    }
-  ])
-});
+      h: 330,
+    },
+  ]);
+}
--- tests/unit/routes/club-test.js
+++ tests/unit/routes/club-test.js
@@ -1,10 +1,10 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | club', function(hooks) {
+module('Unit | Route | club', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let route = this.owner.lookup('route:club');
     assert.ok(route);
   });
