5b938bc711a39b52c1c5fecf2b260ce1cbeb012a
Guillaume Gérard <guillaume.gerard88@gmail.com>
2023-08-22 18:11:02 +0200

    chore: restore stages page

--- app/controllers/links.js
+++ app/controllers/links.js
@@ -39,6 +39,10 @@ export default class LinksController extends Controller {
       url: rootURL + 'assets/aiki3-triptyque-2010-2011.pdf',
     },
     {
+      title: 'Le planning des stages 2023-2024 de la Ligue Grand-Est (pdf)',
+      url: 'https://cloud.aikido-grand-est-ffaaa.fr/s/TDRFWXiLxpYwZLc',
+    },
+    {
       title: 'Le planning des stages 2023-2024 de Patrick Bénézi (pdf)',
       url: 'http://www.aikido-benezi.com/img/affiche/planning%202023-2024%20V1.pdf',
     },
